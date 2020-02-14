5af6f7acfeb614a70b778a35f73d795b1e4fe0d7
Kristoffer X <x@xs-macbook-pro.internal.delfidata.no>
2020-02-14 13:46:21 +0100

    radiopill test

--- src/Husstanden.js
+++ src/Husstanden.js
@@ -1,8 +1,36 @@
-import React, { Component } from "react";
-import { Button, Input, SelectSimple, CheckBox } from "@staccx/bento";
+import React from "react";
+import {
+  Button,
+  Input,
+  SelectSimple,
+  CheckBox,
+  RadioPill,
+  RadioPillItem
+} from "@staccx/bento";
 import VidereKnapp from "./components/VidereKnapp";
 
-export class Husstanden extends Component {
+
+export class Husstanden extends React.Component {
+     myList = [
+        {
+          myUniqueId: "221e98j",
+          value: 1,
+          label: "1",
+          defaultChecked: true
+        },
+        {
+          myUniqueId: "ads31",
+          value: 2,
+          label: "2",
+          defaultChecked: false
+        },
+        {
+          myUniqueId: "h14",
+          value: 3,
+          label: "3 +",
+          defaultChecked: false
+        }
+      ];
   render() {
     return (
       <div>
@@ -12,7 +40,11 @@ export class Husstanden extends Component {
         <Input label={"Etternavn"} />
         <Input label={"Telefonnummer"} />
         <Input label={"E-post"} />
-        <SelectSimple label={"Sivilstatus"} placeholder="Velg..." id="simpleSelect">
+        <SelectSimple
+          label={"Sivilstatus"}
+          placeholder="Velg..."
+          id="simpleSelect"
+        >
           <option value={1}>Gift </option>
           <option value={2}>Samober</option>
           <option value={3}>Skilt</option>
@@ -20,8 +52,27 @@ export class Husstanden extends Component {
           <option value={5}>Enke/Enkemann</option>
           <option value={6}>Separert</option>
         </SelectSimple>
-        <CheckBox id="1ID" group="test">Checkbox 1 </CheckBox>
-        <CheckBox id="2ID" group="test">Checkbox 2 </CheckBox>
+        <p>Har du barn under 18 år ?</p>
+        <CheckBox id="1ID" group="test">
+          Checkbox 1{" "}
+        </CheckBox>
+        <CheckBox id="2ID" group="test">
+          Checkbox 2{" "}
+        </CheckBox>
+        <p>Antall barn</p>
+        <RadioPill group={"Radiopills"}>
+          {myList.map(listItem => (
+            <RadioPillItem
+              key={listItem.myUniqueId}
+              value={listItem.value}
+              defaultChecked={listItem.defaultChecked}
+              id={listItem.myUniqueId}
+            >
+              {listItem.label}
+            </RadioPillItem>
+          ))}
+        </RadioPill>
+
         <VidereKnapp onClick={this.props.onNext} />
       </div>
     );
