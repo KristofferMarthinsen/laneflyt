fef376a4e3ec90d16e95cdd8bbec951d1843b7b7
Helene S <X@Xs-MacBook-Pro.local>
2020-02-20 14:04:01 +0100

    css heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -6,19 +6,15 @@ export class Heading extends Component {
     return (
       <div>
         <Headline>
-            <div className="body">
-                <Title> Lånedetaljer </Title>
-                    <Progress>
-                        <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                    </Progress>
-            </div>
+            <Title> Lånedetaljer</Title>
+                <Progress>
+                        <div className="en"></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                </Progress>
         </Headline>
       </div>
     );
@@ -28,22 +24,40 @@ export class Heading extends Component {
 export default Heading;
 
 const Headline = styled.div`
+    background-color: lightyellow;
+    font-family: Raleway;
+    font-weight: 300;
+    padding-left: 15px;
     display: flex;
     flex-direction: column;
 `
 
 const Title = styled.h1`
-    font-family: Roboto;
-    font-weight: 500;
+    color: #146670;
     font-size: 32px;
+    font-weight: 600;
 `
-const Progress = styled.ul`
-    background-color: grey;
-        li{
-            list-style-type: none;
-            height: 20px;
-            width: 20px;
-            border-radius: 50%;
-            background-color: green;
-        }
+
+const Progress = styled.span`
+    display: flex;
+    justify-content: space-around;
+    width: 140px;
+    div{
+        width: 13px;
+        height: 13px;
+        background-color: #E0E0E0;
+        border-radius: 50%;
+        z-index: 1;
+    }
+    div:hover{
+        background-color: #5C306B;
+    }
+    div.en{
+        background-color: #5C306B;
+        width: 40px; 
+        border-radius: 20px;
+        position: static;
+        z-index: 2;
+        align-self: flex-start;
+    }
 `
