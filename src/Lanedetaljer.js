a060435179c744ae7427f1f3323c7bf2f4408ce2
Kristoffer X <x@xs-macbook-pro.internal.delfidata.no>
2020-02-21 15:34:20 +0100

    add padding lånedetalj

--- src/Lanedetaljer.js
+++ src/Lanedetaljer.js
@@ -10,11 +10,19 @@ export class Lanedetaljer extends Component {
   render() {
     return (
         <div>
-        <Header title="Lånesum" id={0.5}/>
+        <Lånesum>
+        <Header title="Lånedetaljer" id={0.5}/>
+        </Lånesum>
+        <VerdiLaneSumStyle>
         <VerdiLaneSum/>
+        </VerdiLaneSumStyle>
+        <NedbetalingStyle>
         <Nedbetaling/> 
+        </NedbetalingStyle>
+        <Buttons>
        <Link to = "/Lanetakere"><Button>Videre</Button></Link>
         <Link to ="/Nedbetalingsplan"><Button variant="unstyledButton">Nedbetalingsplan</Button></Link>
+        </Buttons>
         </div>
     );
   }
@@ -22,5 +30,25 @@ export class Lanedetaljer extends Component {
 
 export default Lanedetaljer;
 
+const Lånesum = styled.h1`
+padding-top: 124px;
+padding-left: 6px;
+`
 
+const Buttons = styled.div`
+padding-top: 32px;
+display: flex;
+flex-direction: row;
+padding-left: 24px;
+justify-content: space-between;
+`
+const VerdiLaneSumStyle = styled.div`
+padding-top: 64px;
+
+
+`
+const NedbetalingStyle = styled.div`
+padding-top: 32px;
+background: #FFFFFF;
+`
     
--- src/Lanetakere.js
+++ src/Lanetakere.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import VidereKnapp from "./components/VidereKnapp";
 import Header from "./components/Header";
-import { Button,Heading } from "@staccx/bento";
+import { Button, Heading } from "@staccx/bento";
 import { Link } from "react-router-dom";
-import {Gris} from "./svg/Gris"
+import { Gris } from "./svg/Gris";
 export class Lanetakere extends Component {
   render() {
     return (
       <div>
-
-        <Header title="Lånetakere" id={2}/>
+        <Header title="Lånetakere" id={2} />
         {Gris}
         <p>2 stk griser i bilde form</p>
-       <h3> Skal du låne sammen med noen?</h3>
-        <p>En medlåntaker låner sammen med deg for eksempel
-        en samboer kan låne sammen med deg og er
-         f.eksempel 50% ansvarlig for lånet</p>
-        <Button variant = "">Ja</Button>
-        <Button variant = "">Nei</Button>
+        <h3> Skal du låne sammen med noen?</h3>
+        <p>
+          En medlåntaker låner sammen med deg for eksempel en samboer kan låne
+          sammen med deg og er f.eksempel 50% ansvarlig for lånet
+        </p>
+        <Button variant="">Ja</Button>
+        <Button variant="">Nei</Button>
 
         <Link to="/Husstanden">
           <Button>Videre</Button>
