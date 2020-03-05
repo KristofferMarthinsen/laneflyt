9c4d06aecf50f4464452d92333afb0dbe0a70b14
Kristoffer X <x@xs-macbook-pro.internal.delfidata.no>
2020-03-05 13:41:51 +0100

    splittings av form

---
+++ src/components/Form/FormInputs/FornavnInput.js
@@ -0,0 +1,19 @@
+import React from "react"
+import {Input } from "@staccx/bento"
+
+
+
+const FornavnInput = () => {
+
+
+    return (
+        <>
+        <Input
+                type="fornavn"
+                name="fornavn"
+                label="Fornavn"
+              />
+        </>
+    )
+}
+export default FornavnInput
---
+++ src/components/Testytest.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+export const Testytest = () => {
+    return(
+    
+    <div>
+    <p>hei sveis</p>
+    </div>
+    )
+}
+
+
+
+
+export default Testytest;
