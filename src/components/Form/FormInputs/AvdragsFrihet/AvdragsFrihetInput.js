175ddb54f8a966a30ba313f21beaf826dbe21f34
Kristoffer X <x@xs-macbook-pro.home>
2020-03-13 12:30:15 +0100

    lanedetalj formik og localhost

---
+++ src/components/Form/FormInputs/AvdragsFrihet/AvdragsFrihetInput.js
@@ -0,0 +1,13 @@
+import React from "react"
+import AvdragsFrihet from "./AvdragsFrihet";
+
+
+
+const AvdragsFrihetInput = () => {
+	return (
+		<div>
+			<AvdragsFrihet name={"AvdragsFrihet"} label={"Avdragsfrihet"}/>
+		</div>
+	)
+}
+export default AvdragsFrihetInput
