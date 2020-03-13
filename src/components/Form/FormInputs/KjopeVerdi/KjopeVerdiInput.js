6052ee111cd0238b456319a47dbff15ad28cd655
Thomase <thomase@stacc.com>
2020-03-13 09:37:24 +0100

    viker kjopeverdi

--- src/components/Form/FormInputs/KjopeVerdi/KjopeVerdiInput.js
+++ src/components/Form/FormInputs/KjopeVerdi/KjopeVerdiInput.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { SliderKeyboardInput } from "@staccx/bento";
 import {useFormikContext} from "formik"
 
 const KjopeVerdiInput = () => {
+	const [price, setPrice] = useState(0)
 
   const { setFieldValue } = useFormikContext()
   return (
     <div>
       <SliderKeyboardInput
         label="KjopeVerdi"
+		name="KjopeVerdi"
         min={100}
         max={500}
         step={50}
         defaultValue={250}
-        onChange={value => setFieldValue(value)}
+        onChange={value => (setPrice(value), setFieldValue("KjopeVerdi", value))}
       />
     </div>
   );
